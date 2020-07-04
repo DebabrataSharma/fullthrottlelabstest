2d98e18e892dd3ebbec002ab75abefcb96f84056
Debabrata <sharmadeba92@gmail.com>
2020-07-04 11:50:15 +0530

    removed models from app

--- user/models.py
+++ user/models.py
@@ -1,21 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Users(models.Model):
-    real_name = models.CharField(max_length=50)
-    tz = models.CharField(default='Asia/Kolkata', max_length=40)
-
-    def __str__(self):
-        return self.real_name
-
-class ActivityPeriod(models.Model):
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
-    user = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='activity_period')
-
-    class Meta:
-        verbose_name = 'ActivityPeriod'
-        verbose_name_plural = 'ActivityPeriods'
-
-    # def __str__(self):
-    #      return (self.start_time, self.end_time)
